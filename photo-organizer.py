88d6f270e8e51ed51ed7cdf9f24cf9d244a5f01e
Leandro Passos <lpkyrius@hotmail.com>
2021-01-15 15:58:21 -0300

    Preventing a file from being overwritten

--- photo-organizer.py
+++ photo-organizer.py
@@ -3,7 +3,7 @@ import os
 import shutil
 from datetime import datetime
 from PIL import Image
-
+from pathlib import Path
 
 class PhotoOrganizer:
     DATETIME_EXIF_INFO_ID = 36867
@@ -28,9 +28,21 @@ class PhotoOrganizer:
 
     def move_photo(self, file):
         new_folder = self.folder_path_from_photo_date(file)
+        new_path = new_folder + '/' + file
         if not os.path.exists(new_folder):
             os.makedirs(new_folder)
-        shutil.move(file, new_folder + '/' + file)
+        else:
+            # check if file already exists
+            if Path(new_path).exists():
+                increment = 0
+                while True:
+                    increment += 1
+                    filename, file_extension = os.path.splitext(new_path)
+                    new_path = filename + '(' + str(increment) + ')' + file_extension
+                    if not Path(new_path).exists():
+                        break
+
+        shutil.move(file, new_path)
 
     def organize(self):
         photos = [
